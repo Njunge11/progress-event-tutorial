1aa078095b7cbbf28a1c7d177a201f59ddd17c52
Njunge <njungedev@gmail.com>
2018-12-17 21:43:17 +0300

    add node server to handle file uploads

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const formidable = require("formidable");
+const fs = require("fs");
+http
+  .createServer((req, res) => {
+    if (req.url === "/fileupload") {
+      const form = new formidable.IncomingForm();
+      form.parse(req, (err, fields, files) => {
+        console.log("XXXXXXXx", files.file.name);
+        const oldPath = files.file.path;
+        const newPath = "/var/www/html/music/" + files.file.name;
+        fs.rename(oldPath, newPath, err => {
+          if (err) throw err;
+          res.write("File uploaded");
+          res.end();
+        });
+      });
+    }
+  })
+  .listen(8080);
