b2d370c27ad79493041e105910cbfc328279be09
Njunge <njungedev@gmail.com>
2018-12-23 19:19:25 +0300

    added functional progress bar

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-const http = require("http");
-const formidable = require("formidable");
-const fs = require("fs");
-http
-  .createServer((req, res) => {
-      res.setHeader('Access-Control-Allow-Origin', 'http://localhost')
-    if (req.url === "/fileupload") {
-      const form = new formidable.IncomingForm();
-      form.parse(req, (err, fields, files) => {
-        console.log("XXXXXXXx", files.file.name);
-        const oldPath = files.file.path;
-        const newPath = "/var/www/html/music/" + files.file.name;
-        fs.rename(oldPath, newPath, err => {
-          if (err) throw err;
-          res.write("File uploaded");
-          res.end();
-        });
-      });
-    }
-  })
-  .listen(8080);
+// const http = require("http");
+// const formidable = require("formidable");
+// const fs = require("fs");
+// http
+//   .createServer((req, res) => {
+//       res.setHeader('Access-Control-Allow-Origin', 'http://localhost')
+//     if (req.url === "/fileupload") {
+//       const form = new formidable.IncomingForm();
+//       form.parse(req, (err, fields, files) => {
+//           console.log('XXXXXXXXX', files)
+//         const oldPath = files.file.path;
+//         const newPath = "/var/www/html/music/" + files.file.name;
+//         fs.rename(oldPath, newPath, err => {
+//           if (err) throw err;
+//           res.write("File uploaded");
+//           res.end();
+//         });
+//       });
+//     }
+//   })
+//   .listen(8080);
+console.log('1234')
